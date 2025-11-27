bf0006fbe8da2b6a273a46039179531dc7a0718a
gcembalo <gabriele.cembalo02@gmail.com>
2025-11-27 11:03:13 +0100

    create a practice session for ch10

---
+++ 10-PDE/session1/elliptic.cpp
@@ -0,0 +1,32 @@
+// solve the Poisson equation
+//      \pdv[2]{\phi}{x} + \pdv{\phi}{y} - S(x,y) = 0
+// on the unit square 0 ≤ x,y ≤ 1with S = const. and b.c. given by 
+// the exact solution
+// \phi(x,y) = e^{-\pi x}\sin{(-\pi y)} + S/4 (x^2 + y^2)
+//
+// Set NX = NY = 32and try S = 0and then S = 2using Jacobi, Gauss-Seidel and
+// SOR. Compare the number of iterations necessary to achieve convergence,
+//  using the residual and a tolerance of 10-7. Results are given by the
+// following table.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+// Variabili globali (non usare)
+
+// Definizione funzioni
+
+int main(){
+
+    // Implementazione main()
+
+    return 0;
+
+}
+
+
+// ------------------------ Funzioni implementate ------------------------ //
